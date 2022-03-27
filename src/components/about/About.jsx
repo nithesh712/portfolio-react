199376ccd0bff6eb46f9715d83d8e07a86819f55
Nithesh <nithesh.ch712@gmail.com>
2022-03-27 08:14:28 -0700

    about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { GiFastArrow } from 'react-icons/gi'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <img src={ME} alt="" />
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <GiFastArrow className='about__icon' />
+              <h5>Experience</h5>
+              <small>2+ years Exp</small>
+            </article>
+            <article className='about__card'>
+              <GiFastArrow className='about__icon' />
+              <h5>Experience</h5>
+              <small>2+ years Exp</small>
+            </article>
+            <article className='about__card'>
+              <GiFastArrow className='about__icon' />
+              <h5>Experience</h5>
+              <small>2+ years Exp</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quae nulla excepturi ipsa nisi ab nostrum quia dolores facilis repellat vero sequi officiis ratione, possimus totam deleniti voluptatum temporibus qui.</p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
