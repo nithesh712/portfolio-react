0798d36bf967cb96e7968d79faed9b7c8a5ab9f1
Nithesh <nithesh.ch712@gmail.com>
2022-03-27 04:47:32 -0700

    header and nav components

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './nav.css'
+import {AiOutlineHome, AiOutlineUser, AiFillBook, AiOutlineImport} from 'react-icons/ai'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><AiFillBook /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineImport /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineUser /></a>
+    </nav>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDom from 'react-dom';
+import './index.css'
 
 import App from './App';
 
